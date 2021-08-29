b851f664b7a0180426b0879610857561e37b3819
Cody Biggs <codyabiggs@gmail.com>
2021-08-29 12:29:04 -0400

    Addition to shop page and css

--- src/Shop.js
+++ src/Shop.js
@@ -7,9 +7,26 @@ class Shop extends Component {
 		return (
 			<div class="imageContainer">
 				<img class="shopOne" src={jaybird6} alt="headphone" />
-				<p>$149.99</p>
+				<p class="listReasonsTitle">Jaybird Vista - $149.99</p>
+				<ul class="listReasons">
+					<li>16 hour battery life with charging case</li>
+					<li>Music and calls</li>
+					<li>IPX7 Waterproof and Sweatproof</li>
+					<li>Sport Fit</li>
+					<li>JBS1 Wireless Technology</li>
+					<li>Premium Sound with Custom EQ</li>
+				</ul>
+				
 				<img class="shopTwo" src={jaybird7} alt="headphone" />
-				<p>$199.99</p>
+				<p class="listReasonsTitle2">Jaybird Vista 2 - $199.99</p>
+				<ul class="listReasons2">
+					<li>Surround Sense</li>
+					<li>Earthproof Durability</li>
+					<li>IP68 Waterproof and Sweatproof</li>
+					<li>24 Hour Battery</li>
+					<li>Active Noise Cancellation</li>
+					<li>Sport Fit</li>
+				</ul>
 			</div>
 		);
 	}
